d9a094281a138bd832b2d5ec0a8d9fb87c31c772
Dmitrii Ovcharenko <d.ovcharenko@paysend.com>
2023-08-18 01:20:36 +0300

    improve method int_plus_float_to_int

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,6 @@ fn int_plus_float_to_float(a: u32, b: f32) -> f64 {
     (a as f64) + (b as f64)
 }
 
-// функция int_plus_float_to_int принимает 32-х битное целое беззнаковое число и 32-х битное число с плавающей точкой.
-// Возвращает 64-х битное целое беззнаковое число, равное сумме входных.
-fn int_plus_float_to_int(a: u32, b: f32) -> u64 {
-    let first = a as i64;
-    let second = b.trunc() as i64;
-    (first + second) as u64
-}
-
 // функция tuple_sum принимает кортеж из двух целых чисел. Возвращает целое число, равное сумме чисел во входном кортеже.
 fn tuple_sum(tuple: (i64, i64)) -> i64 {
     let (a, b) = tuple;
@@ -45,16 +37,28 @@ fn array_sum(array: [i64; 3]) -> i64 {
     array[0].wrapping_add(array[1]).wrapping_add(array[2])
 }
 
+// функция int_plus_float_to_int принимает 32-х битное целое беззнаковое число и 32-х битное число с плавающей точкой.
+// Возвращает 64-х битное целое беззнаковое число, равное сумме входных.
+fn int_plus_float_to_int(a: u32, b: f32) -> u64 {
+    let first = a as f64;
+    let second = b.trunc() as f64;
+    let sum = first + second;
+    sum.abs() as u64
+}
+
 fn main() {
-    println!("{}", array_sum([1, 2, 3]));
-    println!("{}", double_int32(u32::MAX));
-    println!("{}", double_int64(u32::MAX));
-    println!("{}", double_float32(f32::MAX));
-    println!("{}", double_float64(f32::MAX));
-    println!("{}", int_plus_float_to_float(1_u32, 1.2_f32));
-    println!("{}", tuple_sum((i64::MAX, 2)));
-    println!("{}", array_sum([i64::MAX, i64::MAX, i64::MAX]));
+    // println!("{}", array_sum([1, 2, 3]));
+    // println!("{}", double_int32(u32::MAX));
+    // println!("{}", double_int64(u32::MAX));
+    // println!("{}", double_float32(f32::MAX));
+    // println!("{}", double_float64(f32::MAX));
+    // println!("{}", int_plus_float_to_float(1_u32, 1.2_f32));
+    // println!("{}", tuple_sum((i64::MAX, 2)));
+    // println!("{}", array_sum([i64::MAX, i64::MAX, i64::MAX]));
 
     println!("{}", int_plus_float_to_int(10_u32, -5_f32));
     println!("{}", int_plus_float_to_int(10_u32, -1.5_f32));
+    println!("{}", int_plus_float_to_int(1_u32, -2_f32));
+    println!("{}", int_plus_float_to_int(1_u32, -5.7_f32)); 
+
 }
